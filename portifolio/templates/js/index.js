cfae13fa042f0975f32194a765eaf605bfb0f45b
Arthur Óliver Rossi Alves <arthuroliverrossialves@gmail.com>
2025-05-06 17:08:58 -0300

    feat: adicionando funções do JS para animação

---
+++ portifolio/templates/js/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const introducaoArtigo = document.querySelector(".section_intro");
+  // const Nome_Variável = document.querySelector(".Local_Variável"); caso queira add + item
+
+  // Cria um observador (verifica visibilidade do elemento na tela - se aparece ou não)
+  const observadorDeEntrada = new IntersectionObserver((entradas) => {
+    // Permite a repetição da animação
+    entradas.forEach(entrada => {
+      if (entrada.isIntersecting) {
+        entrada.target.classList.add("slide-in-top");
+      } else {
+        entrada.target.classList.remove("slide-in-top");
+      }
+    });
+  }, {
+    threshold: 0.5 // Ativa animação se 50% estiver visível
+  });
+
+  // Observador criado fica contínuamente analisando o elemento
+  if (introducaoArtigo) {
+    observadorDeEntrada.observe(introducaoArtigo);
+  }
+  // if (Nome_Variável) { caso queira add + item
+  //   observadorDeEntrada.observe(Nome_Variável); caso queira add + item
+  // }
+});
+
+  
